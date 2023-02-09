d7a4dd1a3fcc2d71399faafdb28047c0487cf70f
Yanjiu Li <liyanjiu@outlook.com>
2023-02-09 23:10:42 +0800

    fix merge

--- test/test.h
+++ test/test.h
@@ -13,7 +13,6 @@ void test_log();
 void test_ini();
 void test_base64_md5_sha1();
 
-<<<<<<< HEAD
 // container
 void test_list();
 void test_map();
@@ -22,17 +21,12 @@ void test_rbtree();
 void test_container();
 void test_vector();
 void test_linklist();
-
-=======
 void test_str();
->>>>>>> a3c76dbe8816fb4a782e468d091531eba3c13ec1
 void test_str_split();
 void test_str_trim();
 void test_timer();
 void test_json();
 
-<<<<<<< HEAD
-=======
 // container
 void test_vector();
 void test_map();
@@ -41,7 +35,6 @@ void test_heap();
 void test_array();
 void test_queue();
 
->>>>>>> a3c76dbe8816fb4a782e468d091531eba3c13ec1
 // netbase
 void test_ifi();
 void test_name();
