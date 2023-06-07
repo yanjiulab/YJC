cc993c151c7f955e4e267325068ac13bb49b3416
Yanjiu Li <liyanjiu@outlook.com>
2023-06-07 23:25:13 +0800

    feat: finish auto_cheader.py script

--- script/auto_cheader.py
+++ script/auto_cheader.py
@@ -1,34 +1,32 @@
+#!/bin/python3
+
 import re
+import sys
 
-filename = 'db.c'
-with open(filename, 'r') as f:
-    content = f.read()
 
-# pattern = r'^\w+\s+\**\s+(\w+)\((.*)\)\s*\{'
-pattern = r'^\s*\w+\s+\**\s+(\w+)\((.*)\)\s*\{'
-# pattern = r'^\s*(?:(?:struct|unsigned|signed|const|volatile)\s+)+([\w\s*]+)\**\s*(\w+)\([^\)]*\)\s*\{'
-# pattern = r'^\s*(?:(?:struct|unsigned|signed|const|volatile)\s+)*([\w\s*]+)\**\s*(\w+)\([^\)]*\)\s*\{'
-# 
-# pattern = r'^\s*(?:(?:static)\s+)*([\w\s*]+)\**\s+(\w+)\((.*)\)\s*\{'
-pattern = r'^\s*((struct\s+)?\w+\s*\**)\s+(\w+)\((.*)\)\s*\{'
-# pattern = r'^\s*(?:(?:[\w\s*]+)\s+)*([\w\s*]+)\**\s+(\w+)\((.*)\)\s*\{'
-# pattern = r'^\s*(?:(?:struct|union|enum|\w+\s*\**) +)?([\w\s*]+)\**\s+(\w+)\((.*)\)\s*\{'
-# pattern = r'^\s*(?:(?:struct|union|enum|\w+\s*\**)\s+)?([\w\s*]+)\**\s+(\w+)\((.*)\)\s*\{'
-pattern = r'(\w+)'
+def gen_func_decl():
+    if len(sys.argv) != 2:
+        print("invalid args")
+        exit()
+    filename = sys.argv[1]
+    print("filename: " + filename)
 
-matches = re.findall(pattern, content, re.MULTILINE)
+    with open(filename, 'r') as f:
+        content = f.read()
 
-headername = filename + '.h'
-with open(headername, 'w') as f:
-    for match in matches:
-        print(match)
-        func_return_type, func_name, func_args = match[0], match[1], match[2]
-        f.write(func_return_type.strip(' ') + ' ' + func_name + '(')
-        pattern = r'(\w+\s*\**\s*\w+)'
-        args = re.findall(pattern, func_args)
+    pattern = r'^\s*([\w\s*]+)\**\s+(\w+)\((.*)\)\s*\{'
+    matches = re.findall(pattern, content, re.MULTILINE)
 
-        for i, arg in enumerate(args):
-            f.write(arg)
-            if i != len(args) - 1:
-                f.write(', ')
-        f.write(');\n')
+    headername = filename.split('.')[0] + '.h'
+    print("header: " + headername)
+    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    with open(headername, 'w') as f:
+        for match in matches:
+            func_return_type, func_name, func_args = match[0], match[1], match[2]
+            decl = func_return_type + ' ' + func_name + '(' + func_args + ');\n'
+            f.write(decl)
+            print(decl, end='')
+    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+
+if __name__ == "__main__":
+    gen_func_decl()
